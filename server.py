12485587284c08cb294fb688d4202e649d00a24b
林彥儒 <balilarder@linyanrude-MacBook-Air.local>
2020-02-26 18:29:52 +0800

    simple surveillance function

--- server.py
+++ server.py
@@ -13,11 +13,13 @@ masterQueue = queue.Queue()  # put task
 workerQueue = queue.Queue()  # get result
 ##  still need a queue to get the information of how many workers, their progress 
 dateQueue = queue.Queue()
+infoQueue = queue.Queue()
 
 BaseManager.register('get_master_queue', callable=lambda: masterQueue)
 BaseManager.register('get_worker_queue', callable=lambda: workerQueue)
 
 BaseManager.register('get_date_queue', callable=lambda: dateQueue)
+BaseManager.register('get_info_queue', callable=lambda: infoQueue)
 
 manager = BaseManager(address=('', 5000), authkey=b'abc')
 
@@ -28,6 +30,7 @@ master = manager.get_master_queue()
 worker = manager.get_worker_queue()
 
 date_info = manager.get_date_queue()
+information = manager.get_info_queue()
 
 class Node(object):
     def __init__(self, url, name):
@@ -93,27 +96,59 @@ def get_child(node):
 
 
 def crawler(index):
+    workers = []
     # put url to queue
     date = "2020-02-02"
     index = list(index)
-    for i in index[:60]:
+    for i in index[:100]:
         master.put(i)
     date_info.put(date)
-    # get result from queue
-
-    # sleep 5 second
-
-    # surveillance workers
-
 
     while not master.empty():
-        print("get some thing from result")
+        print("get something from result")
+        
         time.sleep(5)
+        
         print("surveillance")
-
+        message = []
+        while not information.empty():
+            
+            info = information.get()
+            message.append(info)
+            
+        # process surveillance message
+        error = surveillance(message, workers)
+        if error:
+            print("workers are missing")
+            print(error)
+            
+def surveillance(message, workers):
+    # show surveillance message
+    print("show message")
+    for m in message:
+        pid = m[0]
+        info = m[1]
+        print(pid, info)
+    # reverse the list, find how many different workers, if find a new worker, append to workers
+    filter = []
+    catch_worker = []
+    message.reverse()
+    for i in message:
+        if i[0] not in catch_worker:
+            filter.append(i)
+            catch_worker.append(i[0])
     
-
-
+    print(filter)       # message of surveillance!
+    print(catch_worker)
+    for w in catch_worker:
+        if w not in workers:
+            workers.append(w)
+    # if worker decrease, is an error, else return None
+    missing = []
+    for w in workers:
+        if w not in catch_worker:
+            missing.append(w)
+    return missing
 
 if __name__ == '__main__':
     # main()
@@ -127,18 +162,15 @@ if __name__ == '__main__':
         index = []
         has_check = []
         need_to_check = []
-
-        start = 1
-        end = 3
+        # group A~L
+        start = 0
+        end = 12
         gen_all_board(start, end)
         index = set(index)
         with open('boards.pkl', 'wb') as fp:
             pickle.dump(index, fp)
 
-    
-    
     print("start to crawl")
-
     crawler(index)
 
 # # put task into the master queue
--- worker.py
+++ worker.py
@@ -1,11 +1,14 @@
-import time, queue
+import time, queue, random
+import os
 from multiprocessing.managers import BaseManager
+
 
 task_num = 3
 
 BaseManager.register('get_master_queue')
 BaseManager.register('get_worker_queue')
 BaseManager.register('get_date_queue')
+BaseManager.register('get_info_queue')
 
 server_addr = '127.0.0.1'
 manager = BaseManager(address=(server_addr, 5000), authkey=b'abc')
@@ -16,6 +19,7 @@ manager.connect()
 master = manager.get_master_queue()
 worker = manager.get_worker_queue()
 date_info = manager.get_date_queue()
+information = manager.get_info_queue()
 
 def process(n):
     return n+1
@@ -32,14 +36,20 @@ def process(n):
 #     except queue.Empty:
 #         print("task is empty")
 
+pid = os.getpid()
 while not master.empty():
     url = master.get(timeout=100)
-    # result.put((nn, ii, jj, candidates))
     date = date_info.get(timeout=100)
     date_info.put(date)
     print(url)
     print(date)
+    
     time.sleep(1)  # because processing...
+    r = random.randint(1,10)
+    for _ in range(r):
+        print("put")
+        information.put([str(pid), "random:"+str(r)])
+
 print("master is empty")
         
  
